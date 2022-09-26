f556647b647b0a3a0701aa1abe6bd191d2b8cdbd
Chasen <cai981012@163.com>
2022-09-26 22:29:40 +0800

    feat: add eslint and husky

--- packages/eslint-config-base/index.js
+++ packages/eslint-config-base/index.js
@@ -1,4 +1,14 @@
 module.exports = {
+  'extends': [
+    './rules/errors',
+    './rules/strict',
+    './rules/style',
+    './rules/node',
+    './rules/best-practices',
+    './rules/variables',
+    './rules/es6',
+    './rules/imports',
+  ],
   rules:{
 
   }
